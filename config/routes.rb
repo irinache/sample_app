a4d3fe27fec5c8695c488125580657be40f73949
irinache <irina.che333@gmail.com>
2018-11-08 22:45:42 +0200

    Minor improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contacts'
-
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contacts', to: 'static_pages#contacts'
 end
