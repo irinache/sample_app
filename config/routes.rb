32ba68017d2159f9bf652406dcda072bc3c0b12e
irinache <irina.che333@gmail.com>
2018-11-12 22:06:53 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contacts', to: 'static_pages#contacts'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 end
