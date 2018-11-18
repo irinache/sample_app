036a639bff193e95122bd98bf42fa6e122379e12
irinache <irina.che333@gmail.com>
2018-11-18 16:24:42 +0200

    Add showing all users page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,9 +13,27 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contacts_path
+    assert_select "a[href=?]", login_path
     get contacts_path
     assert_select "title", full_title("Contacts")
     get signup_path
     assert_select "title", full_title("Sign up")
+    get about_path
+    assert_select "title", full_title("About")
+    get help_path
+    assert_select "title", full_title("Help")
+
+    log_in_as @user
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    get user_path (@user)
+    assert_select "title", full_title("#{@user.name}")
+    get edit_user_path (@user)
+    assert_select "title", full_title("Edit user")
+    get users_path
+    assert_select "title", full_title("All users")
   end
 end
