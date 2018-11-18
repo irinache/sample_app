6553523e536dbdce756c0d7037d7068d13e3e13f
irinache <irina.che333@gmail.com>
2018-11-18 22:06:09 +0200

    Add account activating

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'example.com' # Don't use this literally; use your local dev host instead
+  host = 'localhost:3000'
   # Use this on the cloud IDE.
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   # Use this if developing on localhost.
   # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
